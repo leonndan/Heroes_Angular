ce821650b9555ec9f1270235fd9ccbe9ac0cf10d
leonndan <daniel.leon5605@alumnos.udg.mx>
2022-03-22 11:27:41 -0600

    listo

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./angular-tour-of-heroes'));
+app.use(express.static('dist/angular-tour-of-heroes'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: './angular-tour-of-heroes/'}),
+    res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes/'}),
 );
 
 // Start the app by listening on the default Heroku port
