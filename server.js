95ad02d0320a8f24c799086c4dd5abd196252fa8
leonndan <daniel.leon5605@alumnos.udg.mx>
2022-03-22 11:39:58 -0600

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('dist/angular-tour-of-heroes'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+ const path = require('path'); 
+ const app = express(); 
+ app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+  app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));});
+   app.listen(process.env.PORT || 8080)
